a3ffd525247c5e5f2d04aab36b090fe268e70f56
Ilhan  Cicek <ilhancicek@Mac.artvin.edu.tr>
2025-05-30 11:47:05 +0300

    “Cc”

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,29 @@ import WalletConnectionProvider from '@/components/WalletConnectionProvider';
 
 export const metadata = {
   title: 'Coincarnation',
-  description: 'Revive your deadcoins.',
+  description: 'Rescue your deadcoins. Coincarnate now!',
+  openGraph: {
+    title: 'Coincarnation',
+    description: 'Revive your deadcoins. Coincarnate them for $MEGY.',
+    url: 'https://megydapp.vercel.app',
+    siteName: 'Coincarnation',
+    images: [
+      {
+        url: '/og-image.png', // görsel buradaysa doğru
+        width: 1200,
+        height: 630,
+        alt: 'Coincarnation Promotional Visual',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Coincarnation',
+    description: 'Trade your deadcoins for $MEGY and join the future.',
+    images: ['/og-image.png'],
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
