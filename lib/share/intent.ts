0a0e29565f9122ce13a7f9b986b792cb356206ef
Ilhan Cicek <ilhancicek@gmail.com>
2025-11-08 15:16:50 +0300

    “Coincarnation”

--- lib/share/intent.ts
+++ lib/share/intent.ts
@@ -1,1 +1,4 @@
-export * from '@/utils/share/intent';
+// lib/share/intent.ts
+// Yeni yönlendirme: paylaşım URL fonksiyonlarını components/share/intent.ts'tan alıyoruz.
+export * from '@/components/share/intent';
+
